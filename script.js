f632a8abb58c2eedcfa18ce2b5a1eac0655cfd71
Vinicius José <viniciusjosedev@gmail.com>
2022-10-14 17:57:54 -0300

    requisito 18 e 19 finalizado

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function init() {
+const btnEnviar = document.getElementById('submit-btn');
+const chkBox = document.getElementById('agreement');
+
+function login() {
 	document.getElementById('login-btn').addEventListener('click', function() {
 		if (document.getElementById('input-email').value = "tryber@teste.com" && document.getElementById('input-password').value === "123456") {
 			alert('Olá, Tryber!')
@@ -7,6 +10,17 @@ function init() {
 			alert('Email ou senha inválidos.')
 		}
 	})
+}
+
+function verifyButton () {
+	if (chkBox.checked) {
+		btnEnviar.disabled = false;
+	}
+}
+
+function init() {
+	login()
+	chkBox.addEventListener('click', verifyButton);
 }
 
 window.onload = init()
