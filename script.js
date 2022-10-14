79a822d2d35bac3c79f8f55dd5111dc30d0de97e
Vinicius José <viniciusjosedev@gmail.com>
2022-10-14 16:49:14 -0300

    requisitos de 1 ao 15 feitos

---
+++ script.js
@@ -0,0 +1,12 @@
+function init() {
+	document.getElementById('submit-btn').addEventListener('click', function() {
+		if (document.getElementById('input-email').value = "tryber@teste.com" && document.getElementById('input-password').value === "123456") {
+			alert('Olá, Tryber!')
+		}
+		else {
+			alert('Email ou senha inválidos.')
+		}
+	})
+}
+
+window.onload = init()
