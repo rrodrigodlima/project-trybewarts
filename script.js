2cf6dcb5a2a3633f2c40a4391c4eb908e68c4275
Vinicius José <viniciusjosedev@gmail.com>
2022-10-16 12:30:11 -0300

    ajustes feitos

--- script.js
+++ script.js
@@ -18,9 +18,41 @@ function verifyButton () {
 	}
 }
 
+function deletP() {
+	if (document.getElementById('counter')) {
+		document.getElementById('counter').remove()
+	}
+}
+
+
+
+function cont(event) {
+	deletP()	
+	let tagP = document.createElement('p')
+	tagP.innerText = (500 - document.getElementsByTagName('textarea')[0].value.length)
+	tagP.id = 'counter'
+	if (document.getElementsByTagName('textarea')[0].value.length === 0) {
+		tagP.style.color = 'green'
+		document.getElementsByClassName('class-3')[0].appendChild(tagP)
+	} else if (document.getElementsByTagName('textarea')[0].value.length > 0 && document.getElementsByTagName('textarea')[0].value.length <= 200) {
+		tagP.style.color = 'green'
+		document.getElementsByClassName('class-3')[0].appendChild(tagP)
+	} else if (document.getElementsByTagName('textarea')[0].value.length > 200 && document.getElementsByTagName('textarea')[0].value.length <= 450) {
+		tagP.style.color = 'blue'
+		document.getElementsByClassName('class-3')[0].appendChild(tagP)
+	} else {
+		tagP.style.color = 'red'
+		document.getElementsByClassName('class-3')[0].appendChild(tagP)
+	}
+	
+}
+
 function init() {
 	login()
 	chkBox.addEventListener('click', verifyButton);
+	document.getElementsByTagName('textarea')[0].addEventListener('keyup', cont)
 }
+
+
 
 window.onload = init()
