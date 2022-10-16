8645a53dfdad744b108e084517a8bb0e506644f1
Vinicius José <viniciusjosedev@gmail.com>
2022-10-16 13:18:04 -0300

    mais alguns ajutes do lint feitos

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
 const btnEnviar = document.getElementById('submit-btn');
 const chkBox = document.getElementById('agreement');
+const inputEmailValue = document.getElementById('input-email').value;
+const inputPassword = document.getElementById('input-password').value;
 
 function login() {
-	document.getElementById('login-btn').addEventListener('click', function() {
-		if (document.getElementById('input-email').value = "tryber@teste.com" && document.getElementById('input-password').value === "123456") {
-			alert('Olá, Tryber!')
-		}
-		else {
-			alert('Email ou senha inválidos.')
-		}
-	})
+  document.getElementById('login-btn').addEventListener('click', () => {
+    if (inputEmailValue === 'tryber@teste.com' && inputPassword === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+  });
 }
 
-function verifyButton () {
-	if (chkBox.checked) {
-		btnEnviar.disabled = false;
-	}
+function verifyButton() {
+  if (chkBox.checked) {
+    btnEnviar.disabled = false;
+  }
 }
 
 function deletP() {
-	if (document.getElementById('counter')) {
-		document.getElementById('counter').remove()
-	}
+  if (document.getElementById('counter')) {
+    document.getElementById('counter').remove();
+  }
 }
 
-
-
-function cont(event) {
-	deletP()	
-	let tagP = document.createElement('p')
-	tagP.innerText = (500 - document.getElementsByTagName('textarea')[0].value.length)
-	tagP.id = 'counter'
-	if (document.getElementsByTagName('textarea')[0].value.length === 0) {
-		tagP.style.color = 'green'
-		document.getElementsByClassName('class-3')[0].appendChild(tagP)
-	} else if (document.getElementsByTagName('textarea')[0].value.length > 0 && document.getElementsByTagName('textarea')[0].value.length <= 200) {
-		tagP.style.color = 'green'
-		document.getElementsByClassName('class-3')[0].appendChild(tagP)
-	} else if (document.getElementsByTagName('textarea')[0].value.length > 200 && document.getElementsByTagName('textarea')[0].value.length <= 450) {
-		tagP.style.color = 'blue'
-		document.getElementsByClassName('class-3')[0].appendChild(tagP)
-	} else {
-		tagP.style.color = 'red'
-		document.getElementsByClassName('class-3')[0].appendChild(tagP)
-	}
-	
+function cont() {
+  deletP();
+  const tagP = document.createElement('p');
+  tagP.innerText = (500 - document.getElementsByTagName('textarea')[0].value.length);
+  tagP.id = 'counter';
+  if (document.getElementsByTagName('textarea')[0].value.length > 0) {
+    tagP.style.color = 'black';
+    document.getElementsByClassName('class-3')[0].appendChild(tagP);
+  }
+  // } else if (document.getElementsByTagName('textarea')[0].value.length > 0 && document.getElementsByTagName('textarea')[0].value.length <= 200) {
+  //   tagP.style.color = 'black';
+  //   document.getElementsByClassName('class-3')[0].appendChild(tagP);
+  // } else if (document.getElementsByTagName('textarea')[0].value.length > 200 && document.getElementsByTagName('textarea')[0].value.length <= 450) {
+  //   tagP.style.color = 'blue';
+  //   document.getElementsByClassName('class-3')[0].appendChild(tagP);
+  // } else {
+  //   tagP.style.color = 'red';
+  //   document.getElementsByClassName('class-3')[0].appendChild(tagP);
+  // }
 }
 
 function init() {
-	login()
-	chkBox.addEventListener('click', verifyButton);
-	document.getElementsByTagName('textarea')[0].addEventListener('keyup', cont)
+  login();
+  chkBox.addEventListener('click', verifyButton);
+  document.getElementsByTagName('textarea')[0].addEventListener('keyup', cont);
 }
 
-
-
-window.onload = init()
+window.onload = init;
