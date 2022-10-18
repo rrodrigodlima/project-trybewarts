98623f9b69ae7eda257e7dd6503ea7f78c419105
Vinicius José <viniciusjosedev@gmail.com>
2022-10-18 08:54:27 -0300

    script.js sendo ajustado

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const inputPassword = document.getElementById('input-password');
 const textareaGet = document.getElementsByTagName('textarea')[0];
 let numLista = 0;
 let num = 0;
-let listaInfo = {};
+const listaInfo = {};
 let teste = 0;
-let lista = []
+const lista = [];
 
 function login() {
   document.getElementById('login-btn').addEventListener('click', () => {
@@ -24,14 +24,13 @@ function login() {
 function verifyButton() {
   if (num % 2 === 0) {
     btnEnviar.disabled = false;
-		btnEnviar.style.backgroundColor = 'rgb(81, 74, 146)'
-		num += 1;
-
+    btnEnviar.style.backgroundColor = 'rgb(81, 74, 146)';
+    num += 1;
   } else {
-		btnEnviar.disabled = true;
-		btnEnviar.style.backgroundColor = 'grey'
-		num += 1;
-	}
+    btnEnviar.disabled = true;
+    btnEnviar.style.backgroundColor = 'grey';
+    num += 1;
+  }
 }
 
 function deletP() {
@@ -63,71 +62,71 @@ function cont() {
 
 function deleteMain() {
   // document.getElementsByTagName('main')[0].remove()
-	document.getElementById('evaluation-form').style.display = 'none'
+  document.getElementById('evaluation-form').style.display = 'none';
 }
 
 function verificacao() {
   if (teste === 0) {
-		document.getElementsByTagName('main')[0].style.flexDirection = 'row-reverse';
-		teste += 1;
-	}
+    document.getElementsByTagName('main')[0].style.flexDirection = 'row-reverse';
+    teste += 1;
+  }
 }
 
 function nextPage() {
   let index = 0;
-	for (index = 0; index < 7; index += 1) {
-		let tagP = document.createElement('p');
-		tagP.className = 'p-final-info'
-		document.getElementById('form-data').appendChild(tagP);
-		console.log('teste')
-		verificacao()
-	}
-	document.getElementsByClassName('p-final-info')[0].innerText = `Nome: ${listaInfo['name']} ${listaInfo['lastname']}`
-	document.getElementsByClassName('p-final-info')[1].innerText = `Email: ${listaInfo['email']}`
-	document.getElementsByClassName('p-final-info')[2].innerText = `Casa: ${listaInfo['casa']}`
-	document.getElementsByClassName('p-final-info')[3].innerText = `Família: ${listaInfo['family']}`
-	document.getElementsByClassName('p-final-info')[4].innerText = `Matérias:${listaInfo['check']}`
-	document.getElementsByClassName('p-final-info')[5].innerText = `Avaliação: ${listaInfo['rate']}`
-	document.getElementsByClassName('p-final-info')[6].innerText = `Observações: ${listaInfo['textarea']}`
+  for (index = 0; index < 7; index += 1) {
+    const tagP = document.createElement('p');
+    tagP.className = 'p-final-info';
+    document.getElementById('form-data').appendChild(tagP);
+    console.log('teste');
+    verificacao();
+  }
+  const pFinal = document.getElementsByClassName('p-final-info');
+  pFinal[0].innerText = `Nome: ${listaInfo.name} ${listaInfo.lastname}`;
+  pFinal[1].innerText = `Email: ${listaInfo.email}`;
+  pFinal[2].innerText = `Casa: ${listaInfo.casa}`;
+  pFinal[3].innerText = `Família: ${listaInfo.family}`;
+  pFinal[4].innerText = `Matérias:${listaInfo.check}`;
+  pFinal[5].innerText = `Avaliação: ${listaInfo.rate}`;
+  pFinal[6].innerText = `Observações: ${listaInfo.textarea}`;
 }
 
 function saveInfo() {
   let index = 0;
-	for (index = 0; index < (document.getElementsByClassName('saved-input-1').length - 1); index += 1) {
-		numLista = index;
-		if (document.getElementsByClassName('saved-input-1')[numLista].type === 'radio') {
-			if (document.getElementsByClassName('saved-input-1')[numLista].checked === true) {
-				// listaInfo.push(document.getElementsByClassName('saved-input-1')[numLista].value)
-				listaInfo[document.getElementsByClassName('saved-input-1')[numLista].name] = document.getElementsByClassName('saved-input-1')[numLista].value
-			}
-		} else if (document.getElementsByClassName('saved-input-1')[numLista].type === 'checkbox') {
-			if (document.getElementsByClassName('saved-input-1')[numLista].checked === true) {
-				lista.push(' ' + document.getElementsByClassName('saved-input-1')[numLista].value)
-				listaInfo[document.getElementsByClassName('saved-input-1')[numLista].name] = lista
-				console.log(lista)
-			}
-		} else {
-			listaInfo[document.getElementsByClassName('saved-input-1')[numLista].name] = document.getElementsByClassName('saved-input-1')[numLista].value
-		}
-	}
-	console.log(listaInfo)
+  for (index = 0; index < (document.getElementsByClassName('saved-input-1').length - 1); index += 1) {
+    numLista = index;
+    if (document.getElementsByClassName('saved-input-1')[numLista].type === 'radio') {
+      if (document.getElementsByClassName('saved-input-1')[numLista].checked === true) {
+        // listaInfo.push(document.getElementsByClassName('saved-input-1')[numLista].value)
+        listaInfo[document.getElementsByClassName('saved-input-1')[numLista].name] = document.getElementsByClassName('saved-input-1')[numLista].value;
+      }
+    } else if (document.getElementsByClassName('saved-input-1')[numLista].type === 'checkbox') {
+      if (document.getElementsByClassName('saved-input-1')[numLista].checked === true) {
+        lista.push(` ${document.getElementsByClassName('saved-input-1')[numLista].value}`);
+        listaInfo[document.getElementsByClassName('saved-input-1')[numLista].name] = lista;
+        console.log(lista);
+      }
+    } else {
+      listaInfo[document.getElementsByClassName('saved-input-1')[numLista].name] = document.getElementsByClassName('saved-input-1')[numLista].value;
+    }
+  }
 }
 
 function submitButton(event) {
   event.preventDefault();
-	saveInfo()
+  saveInfo();
   deleteMain();
-  let tagSection = document.createElement('section')
-  tagSection.id = 'form-data'
-  document.getElementsByTagName('main')[0].appendChild(tagSection)
-  nextPage()
+  const tagSection = document.createElement('section');
+  tagSection.id = 'form-data';
+  document.getElementsByTagName('main')[0].appendChild(tagSection);
+  nextPage();
 }
 
 function init() {
   login();
   chkBox.addEventListener('click', verifyButton);
   document.getElementsByTagName('textarea')[0].addEventListener('keyup', cont);
-	document.getElementById('submit-btn').addEventListener('click', submitButton)
+  document.getElementById('submit-btn').addEventListener('click', submitButton);
 }
 
 window.onload = init;
